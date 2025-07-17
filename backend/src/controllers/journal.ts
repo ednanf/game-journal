9d6bd372ed4119a5d58963b8ee7d5255a0ce4f45
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-07-17 15:46:13 -0300

    refactor: Improve formatting and structure of journal entry responses

--- backend/src/controllers/journal.ts
+++ backend/src/controllers/journal.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { StatusCodes} from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 import JournalEntry from '../models/JournalEntry.js';
 
 const getJournalEntries = async (req: Request, res: Response) => {
@@ -9,20 +9,23 @@ const getJournalEntries = async (req: Request, res: Response) => {
 
   const entries = await JournalEntry.find({ createdBy: req.user.userId });
 
-  return res.status(StatusCodes.OK).json({status: 'success', data: { journalEntries: entries } });
+  return res.status(StatusCodes.OK).json({
+    status: 'success',
+    data: { journalEntries: entries },
+  });
 };
 
 const getJournalEntry = async (req: Request, res: Response) => {
   const userId = req.user?.userId;
   const entryId = req.params.id;
 
-  const entry = await JournalEntry.findOne({_id: entryId, createdBy: userId });
+  const entry = await JournalEntry.findOne({ _id: entryId, createdBy: userId });
 
   if (!entry) {
     return res.status(StatusCodes.NOT_FOUND).json({ error: `Entry with id ${entryId} not found` });
   }
 
-  return res.status(StatusCodes.OK).json({status: 'success', data: entry});
+  return res.status(StatusCodes.OK).json({ status: 'success', data: entry });
 };
 
 const createJournalEntry = async (req: Request, res: Response) => {
@@ -33,16 +36,37 @@ const createJournalEntry = async (req: Request, res: Response) => {
   // Don't modify req.body, create a new object
   const journalData = {
     ...req.body,
-    createdBy: req.user.userId
+    createdBy: req.user.userId,
   };
 
   const newJournalEntry = await JournalEntry.create(journalData);
 
-  return res.status(StatusCodes.CREATED).json({status: 'success', data: { journalEntry: newJournalEntry } });
+  return res.status(StatusCodes.CREATED).json({
+    status: 'success',
+    data: { journalEntry: newJournalEntry },
+  });
 };
 
-const updateJournalEntry = (req: Request, res: Response) => {
-  res.status(200).json({ message: 'update journal entry' });
+const updateJournalEntry = async (req: Request, res: Response) => {
+  const userId = req.user?.userId;
+  const entryId = req.params.id;
+
+  const contents = req.body;
+
+  const updatedEntry = await JournalEntry.findOneAndUpdate(
+    {
+      _id: entryId,
+      createdBy: userId,
+    },
+    contents,
+    { new: true, runValidators: true },
+  );
+
+  if (!updatedEntry) {
+    return res.status(StatusCodes.NOT_FOUND).json({ error: `Entry with id ${entryId} not found` });
+  }
+
+  return res.status(StatusCodes.OK).json({status: 'success', data: updatedEntry });
 };
 
 const deleteJournalEntry = (req: Request, res: Response) => {
