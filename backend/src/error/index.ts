c71a75a291c98645e5ef3465c240c7d2cbb80066
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-07-26 10:02:07 -0300

    feat: Add centralized error handling module with custom error exports
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

---
+++ backend/src/error/index.ts
@@ -0,0 +1,8 @@
+// Centralized error handling module
+
+export { default as CustomError } from './CustomError.js';
+export { default as HttpError } from './HttpError.js';
+export { default as NotFound } from './NotFound.js';
+export { default as BadRequest } from './BadRequest.js';
+export { default as Unauthenticated } from './Unauthenticated.js';
+export { default as DatabaseError } from './DatabaseError.js';
