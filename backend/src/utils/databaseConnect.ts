239cd565af0764304330a571d1bdb414c86fa079
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-07-26 09:04:34 -0300

    fix: Enhance databaseConnect function to throw error if MONGODB_URI is missing
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- backend/src/utils/databaseConnect.ts
+++ backend/src/utils/databaseConnect.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
-const databaseConnect = async (uri: string | undefined) => {
-  if (typeof uri === 'string') {
-    await mongoose.connect(uri);
+const databaseConnect = async (uri: string | undefined): Promise<void> => {
+  if (!uri) {
+    throw new Error(
+      '[system] MONGODB_URI is missing. Please set it in your environment variables.',
+    );
   }
-  console.log('[system] connected to MongoDB...');
+  await mongoose.connect(uri);
+  console.log('[system] successfully connected to MongoDB...');
 };
 
 export default databaseConnect;
