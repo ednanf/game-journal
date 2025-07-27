75f334676192d54bcfb9fc66e14e88b19a13f668
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-07-27 11:11:21 -0300

    chore: Change error classes comments
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- backend/src/errors/NotFoundError.ts
+++ backend/src/errors/NotFoundError.ts
@@ -3,6 +3,7 @@ import HttpError from './HttpError.js';
 
 // Custom errors class for Not Found (HTTP 404)
 // This class extends HttpError and sets the status code to 404.
+// It is used to indicate that the requested resource could not be found on the server.
 class NotFoundError extends HttpError {
   constructor(message: string) {
     super(StatusCodes.NOT_FOUND, message);
