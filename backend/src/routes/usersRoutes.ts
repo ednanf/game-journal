fb18535c9ac5d50ea406278ef7ec7b0f1730ca7e
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-07-28 10:27:28 -0300

    feat: Add requiresAuthentication middleware to deleteUser route
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { registerUser, loginUser, logoutUser, deleteUser } from '../controllers/usersController.js';
+import requiresAuthentication from '../middlewares/requiresAuthentication.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
-router.delete('/delete', deleteUser);
+router.delete('/delete', requiresAuthentication, deleteUser);
 
 export default router;
