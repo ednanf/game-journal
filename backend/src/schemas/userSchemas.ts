6017b2dfa20807d599f528bd3391c784b6146bf2
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-07-30 12:51:34 -0300

    feat: Add Zod schemas for validating user registration and login data
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

---
+++ backend/src/schemas/userSchemas.ts
@@ -0,0 +1,20 @@
+// This module defines the schemas for validating user data.
+// It uses Zod for schema validation, ensuring that the data conforms to the expected structure.
+import { z } from 'zod';
+
+// This defines the shape of the request body for registering a new user.
+const registerUserBodySchema = z.object({
+  email: z.email({
+    error: 'Email must be a string',
+  }),
+  password: z
+    .string({
+      error: 'Password must be a string',
+    })
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+});
+
+// The loginUserBodySchema is the same as registerUserBodySchema, therefore no need to redefine it.
+const loginUserBodySchema = registerUserBodySchema;
+
+export { registerUserBodySchema, loginUserBodySchema };
