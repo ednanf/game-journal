7d1eef4a23e1f2103165b8f5dfc532f7e14b7969
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-07-30 12:51:39 -0300

    feat: Add Zod validation middleware to user registration and login routes
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { registerUser, loginUser, logoutUser, deleteUser } from '../controllers/usersController.js';
 import authenticate from '../middlewares/authenticate.js';
+import validateZodSchemas from '../middlewares/validateZodSchemas.js';
+import { registerUserBodySchema, loginUserBodySchema } from '../schemas/userSchemas.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', validateZodSchemas(registerUserBodySchema), registerUser);
+router.post('/login', validateZodSchemas(loginUserBodySchema), loginUser);
 router.post('/logout', logoutUser);
 router.delete('/delete', authenticate, deleteUser);
 
