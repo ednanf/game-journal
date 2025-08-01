c5b48ada5f8a1983641eea9badc6fc0460355882
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-01 10:25:08 -0300

    feat(button): add `Button` component with customizable styles and functionality
    - Introduced a new `Button` component that supports different colors and icons.
    - Implemented styles in `Button.module.css` for consistent button appearance.
    - The component can function as a link or a button based on the presence of an `onClick` handler.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.css';
+
+// default color = purple
+type ButtonProps = {
+  to: string;
+  icon?: React.ReactNode;
+  color: 'default' | 'cyan' | 'green' | 'magenta' | 'yellow';
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+const Button = ({ to, icon, color, onClick, children }: ButtonProps) => {
+  const colorClass =
+    color === 'default' ? styles.button : `${styles.button} ${styles[`button--${color}`]}`;
+
+  // Buttons that require an onClick handler are not links
+  if (onClick) {
+    return (
+      <button className={colorClass} onClick={onClick} type="button">
+        <span className={styles.icon}>{icon}</span>
+        <span className={styles.text}>{children}</span>
+      </button>
+    );
+  }
+
+  return (
+    <Link to={to} className={colorClass}>
+      <span className={styles.icon}>{icon}</span>
+      <span className={styles.text}>{children}</span>
+    </Link>
+  );
+};
+export default Button;
