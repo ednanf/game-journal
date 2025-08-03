86775a061a488dfa0ec85a8e2d4851cfa00a50c1
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-03 10:38:34 -0300

    feat(LoginPage): implement login form with validation and error handling
    - Added state management for form data and errors.
    - Implemented `handleChange` and `handleSubmit` functions for form handling.
    - Included basic validation for email and password fields.
    - Integrated success and error notifications using `react-toastify`.
    - Updated UI elements to reflect changes in form structure and error handling.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,30 +1,119 @@
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { Link, useNavigate } from 'react-router-dom';
+import { postUnwrapped } from '../../utils/axiosInstance.ts';
+import { API_BASE_URL } from '../../config/apiURL.ts';
 import TextInput from '../../components/Form/TextInput/TextInput.tsx';
 import Button from '../../components/Button/Button.tsx';
 import sharedStyles from '../shared.module.css';
 import styles from './LoginPage.module.css';
 
+type FormData = {
+  email: string;
+  password: string;
+};
+
+// No need to use the correct response structure as it was unwrapped in the axios interceptor
+interface RegisterResponse {
+  message: string;
+  user: string;
+  token: string;
+}
+
+interface ApiError {
+  message: string;
+}
+
 const LoginPage = () => {
-  // TODO: Implement handleChange and handleSubmit functions for form handling
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState<FormData>({
+    email: '',
+    password: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Reset errors
+    setErrors({ email: '', password: '' });
+
+    // Basic validation
+    if (!formData.email) {
+      setErrors((prev) => ({ ...prev, email: 'Email is required' }));
+      return;
+    }
+    if (!formData.password) {
+      setErrors((prev) => ({ ...prev, password: 'Password is required' }));
+      return;
+    }
+
+    try {
+      if (!errors.email && !errors.password) {
+        // The response was unwrapped in the axios interceptor
+        const response = (await postUnwrapped(
+          `${API_BASE_URL}/users/login`,
+          formData,
+        )) as RegisterResponse;
+
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('user', response.user);
+
+        toast.success('Login successful! Welcome back!');
+
+        navigate('/journal');
+      }
+    } catch (error) {
+      const apiError = error as ApiError;
+      toast.error(apiError.message || 'An error occurred. Please try again.');
+    }
+  };
+
   return (
     <div className={sharedStyles.pageContainer}>
       <div className={sharedStyles.titleContainer}>
-        <h2>Log In</h2>
+        <h2>Sign up</h2>
       </div>
       <div className={styles.formContent}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.formInputs}>
-            <TextInput label={'Email'} id={'email'} name={'email'} type={'email'} />
-            <TextInput label={'Password'} id={'password'} name={'password'} type={'password'} />
+            <TextInput
+              label={'Email'}
+              id={'email'}
+              name={'email'}
+              type={'email'}
+              onChange={handleChange}
+              required={true}
+              error={errors.email}
+            />
+            <TextInput
+              label={'Password'}
+              id={'password'}
+              name={'password'}
+              type={'password'}
+              onChange={handleChange}
+              required={true}
+              error={errors.password}
+            />
           </div>
           <div className={styles.formButton}>
-            <Button color={'default'}>{'Log In'}</Button>
+            <Button type={'submit'} color={'default'}>
+              {'Log in'}
+            </Button>
           </div>
         </form>
       </div>
       <div className={styles.footer}>
         <p>
-          No account? <Link to={'../signup'}>Sign up.</Link>
+          Don't have an account? <Link to={'../login'}>Sign up.</Link>
         </p>
       </div>
     </div>
