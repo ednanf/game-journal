9460c2425fb7a9abf8793354e923d118b557ce46
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-04 10:51:14 -0300

    feat(platforms): add gaming platforms and game status options
    - Introduced `gamingPlatforms` array for drop-down menu with various gaming platforms.
    - Added `gameStatus` array to define status options for games in the application.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

---
+++ frontend/src/data/status.ts
@@ -0,0 +1,8 @@
+// This file contains the status options for games in the application.
+// They follow the format accepted by the backend model JournalEntry.
+
+export const gameStatus: { label: string; value: string }[] = [
+  { label: 'Started', value: 'started' },
+  { label: 'Completed', value: 'completed' },
+  { label: 'Dropped', value: 'dropped' },
+];
