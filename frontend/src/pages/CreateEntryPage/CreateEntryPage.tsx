e2aede4049417bec7319de265a2ada36834047f0
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-04 10:54:46 -0300

    feat(CreateEntryPage): implement form layout and state management
    - Added a new CSS class `.formContent` for styling the form layout.
    - Integrated `TextInput` and `DropDown` components for user input.
    - Established state management for form data using React hooks.
    - Enhanced user experience by organizing form fields in a structured manner.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/pages/CreateEntryPage/CreateEntryPage.tsx
+++ frontend/src/pages/CreateEntryPage/CreateEntryPage.tsx
@@ -1,11 +1,64 @@
+import React, { useState } from 'react';
+import TextInput from '../../components/Form/TextInput/TextInput.tsx';
+import DropDown from '../../components/Form/DropDown/DropDown.tsx';
+import { gamingPlatforms } from '../../data/platforms.ts';
+import { gameStatus } from '../../data/status.ts';
 import styles from './CreateEntryPage.module.css';
 import sharedStyles from '../shared.module.css';
 
+type FormData = {
+  title: string;
+  platform: string;
+  status: string;
+  rating: number;
+};
+
 const CreateEntryPage = () => {
+  const [formData, setFormData] = useState<FormData>({
+    title: '',
+    platform: '',
+    status: '',
+    rating: 5,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    // Extract name (name of the form field) and value from the event
+    const { name, value } = e.target;
+    setFormData((previousData) => ({
+      ...previousData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className={sharedStyles.pageContainer}>
-      <div className={styles.createEntryContent}>
+      <div className={sharedStyles.titleContainer}>
         <h2>Create Entry</h2>
+      </div>
+      <div className={styles.formContent}>
+        <form>
+          <TextInput label={'Title'} id={'title'} name={'title'} type={'text'} />
+          <DropDown
+            label={'Platform'}
+            id={'platform'}
+            name={'platform'}
+            onChange={handleChange}
+            size={1}
+            value={formData.platform}
+            values={gamingPlatforms}
+            placeholder={'Select a platform...'}
+          />
+          <DropDown
+            label={'Status'}
+            id={'status'}
+            name={'status'}
+            onChange={handleChange}
+            size={1}
+            value={formData.status}
+            values={gameStatus}
+            placeholder={'Select status...'}
+          />
+        </form>
       </div>
     </div>
   );
