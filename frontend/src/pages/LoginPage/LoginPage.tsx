f2c65e34c3c4d21ae434e29da1f8a8ff97bea91e
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-04 13:33:03 -0300

    fix(LoginPage): reset errors on form submission
    * Ensures that error messages for email and password fields are cleared when the form is submitted.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -54,6 +54,7 @@ const LoginPage = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     // Reset errors
     setErrors({ email: '', password: '' });
 
