746649345e5d20a7c354b4a2261bc07f12bed272
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-11 11:02:58 -0300

    feat(StatisticsPage): enhance loading state and improve statistics display
    - Added a loading state with a centered loader while fetching statistics.
    - Updated the statistics display to dynamically show data from the API.
    - Included padding in CSS for better layout consistency.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/pages/StatisticsPage/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage/StatisticsPage.tsx
@@ -1,46 +1,108 @@
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { getUnwrapped } from '../../utils/axiosInstance.ts';
 import LifetimeCard from '../../components/Statistics/LifetimeCard/LifetimeCard.tsx';
 import YearlyCard from '../../components/Statistics/YearlyCard/YearlyCard.tsx';
-import styles from './StatisticsPage.module.css';
-import sharedStyles from '../shared.module.css';
 import { TbPacman } from 'react-icons/tb';
 import { GoDotFill } from 'react-icons/go';
 import { PiGhostBold } from 'react-icons/pi';
+import styles from './StatisticsPage.module.css';
+import sharedStyles from '../shared.module.css';
+
+type Statistics = {
+  lifetime: {
+    completed: number;
+    started: number;
+    paused: number;
+    revisited: number;
+    dropped: number;
+  };
+  byYear: {
+    [year: string]: {
+      completed: number;
+      started: number;
+      paused: number;
+      revisited: number;
+      dropped: number;
+    };
+  };
+};
 
 const StatisticsPage = () => {
+  const [statistics, setStatistics] = useState<Statistics>({
+    lifetime: {
+      completed: 0,
+      started: 0,
+      paused: 0,
+      revisited: 0,
+      dropped: 0,
+    },
+    byYear: {},
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchStatistics = async () => {
+      setIsLoading(true);
+      try {
+        const response = await getUnwrapped<Statistics>('/journal-entries/statistics');
+        setStatistics(response);
+      } catch (err) {
+        setError((err as Error).message);
+        toast.error(`Error fetching statistics: ${error}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    void fetchStatistics();
+  }, [error]);
+
   return (
     <div className={sharedStyles.pageContainer}>
       <div className={sharedStyles.titleContainer}>
         <h2>Statistics</h2>
       </div>
       <div className={sharedStyles.pageContent}>
-        <div className={styles.lifetimeCardContainer}>
-          <LifetimeCard
-            completed={3}
-            dropped={3}
-            paused={3}
-            revisited={3}
-            started={3}
-            title={'Lifetime'}
-          />
-        </div>
-        <div className={styles.yearlyCardContainer}>
-          <YearlyCard
-            completed={4}
-            dropped={4}
-            paused={4}
-            revisited={4}
-            started={4}
-            title={'2024'}
-          />
-        </div>
-        <div>
-          <p className={styles.endMessageDecoration}>
-            <TbPacman size={30} /> <GoDotFill /> <GoDotFill /> <GoDotFill /> <GoDotFill />
-            <GoDotFill /> <GoDotFill /> <GoDotFill />
-            <PiGhostBold size={30} />
-          </p>
-          <p className={styles.endMessage}>Nothing to see beyond here...</p>
-        </div>
+        {isLoading ? (
+          <div className={styles.centerLoader}>
+            <div className={sharedStyles.loader}></div>
+          </div>
+        ) : (
+          <>
+            <div className={styles.lifetimeCardContainer}>
+              <LifetimeCard
+                completed={statistics.lifetime.completed}
+                dropped={statistics.lifetime.dropped}
+                paused={statistics.lifetime.paused}
+                revisited={statistics.lifetime.revisited}
+                started={statistics.lifetime.started}
+                title={'Lifetime'}
+              />
+            </div>
+            {Object.entries(statistics.byYear).map(([year, stats]) => (
+              <div className={styles.yearlyCardContainer} key={year}>
+                <YearlyCard
+                  completed={stats.completed}
+                  dropped={stats.dropped}
+                  paused={stats.paused}
+                  revisited={stats.revisited}
+                  started={stats.started}
+                  title={year}
+                />
+              </div>
+            ))}
+            <div>
+              <p className={styles.endMessageDecoration}>
+                <TbPacman size={30} /> <GoDotFill /> <GoDotFill /> <GoDotFill /> <GoDotFill />
+                <GoDotFill /> <GoDotFill /> <GoDotFill />
+                <PiGhostBold size={30} />
+              </p>
+              <p className={styles.endMessage}>Nothing to see beyond here...</p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
