e9c8ccf96173928ebab9506f17743e215b4f5f55
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-11 11:50:23 -0300

    feat(ErrorPage): enhance error page layout and styling
    * Added new styles for the error page to improve user experience.
    * Updated the error message display with a warning icon and clearer messaging.
    * Removed unused loader styles from shared module for better maintainability.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,11 +1,23 @@
+import { Link } from 'react-router-dom';
+import { PiWarningOctagon } from 'react-icons/pi';
 import styles from './ErrorPage.module.css';
 import sharedStyles from '../shared.module.css';
 
 const ErrorPage = () => {
   return (
-    <div className={sharedStyles.pageContainer}>
-      <div className={styles.errorPageContent}>
-        <h2>ERROR 404</h2>
+    <div className={styles.pageContent}>
+      <PiWarningOctagon size={80} />
+      <div className={styles.titleContainer}>
+        <h2>404 - Page Not Found</h2>
+      </div>
+      <div className={styles.errorMessage}>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <p>
+          Please check the URL or{' '}
+          <Link to={'/'} className={sharedStyles.link}>
+            return to the homepage.
+          </Link>
+        </p>
       </div>
     </div>
   );
