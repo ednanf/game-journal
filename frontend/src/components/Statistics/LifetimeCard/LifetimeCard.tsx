9d05ff915f8ac908168729823bb26e62743646d6
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-11 09:40:56 -0300

    feat(StatisticsPage): add LifetimeCard component with ItemCard integration
    - Introduced `LifetimeCard` to display statistics with individual `ItemCard` components for each status.
    - Created corresponding CSS modules for styling the new components.
    - Updated `StatisticsPage` to include the `LifetimeCard` for a comprehensive statistics overview.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

---
+++ frontend/src/components/Statistics/LifetimeCard/LifetimeCard.tsx
@@ -0,0 +1,42 @@
+import ItemCard from '../ItemCard/ItemCard.tsx';
+import styles from './LifetimeCard.module.css';
+
+type LifetimeCardProps = {
+  title: string;
+  started: number;
+  completed: number;
+  dropped: number;
+  paused: number;
+  revisited: number;
+};
+
+const LifetimeCard = ({
+  title,
+  started,
+  completed,
+  dropped,
+  paused,
+  revisited,
+}: LifetimeCardProps) => (
+  <div className={styles.cardContainer}>
+    <div className={styles.cardHeader}>
+      <h3>{title}</h3>
+    </div>
+    <div className={styles.cardContentGrid}>
+      {/* First row */}
+      <ItemCard title="Started" value={started} color="started" />
+      <div className={styles.divider} />
+      <ItemCard title="Completed" value={completed} color="completed" />
+      <div className={styles.divider} />
+      <ItemCard title="Dropped" value={dropped} color="dropped" />
+      {/* Second row */}
+      <ItemCard title="Paused" value={paused} color="paused" />
+      <div className={styles.divider} />
+      <ItemCard title="Revisited" value={revisited} color="revisited" />
+      <div className={styles.divider} />
+      {/* Empty cell at gridColumn: 5, gridRow: 2 */}
+    </div>
+  </div>
+);
+
+export default LifetimeCard;
--- frontend/src/pages/StatisticsPage/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage/StatisticsPage.tsx
@@ -1,11 +1,25 @@
-import sharedStyles from '../shared.module.css';
+import LifetimeCard from '../../components/Statistics/LifetimeCard/LifetimeCard.tsx';
 import styles from './StatisticsPage.module.css';
+import sharedStyles from '../shared.module.css';
 
 const StatisticsPage = () => {
   return (
     <div className={sharedStyles.pageContainer}>
-      <div className={styles.statisticsContent}>
+      <div className={sharedStyles.titleContainer}>
         <h2>Statistics</h2>
+      </div>
+      <div className={sharedStyles.pageContent}>
+        <div className={styles.lifetimeCardContainer}>
+          <LifetimeCard
+            completed={3}
+            dropped={3}
+            paused={3}
+            revisited={3}
+            started={3}
+            title={'Lifetime'}
+          />
+        </div>
+        <div>Yearly Statics Cards</div>
       </div>
     </div>
   );
