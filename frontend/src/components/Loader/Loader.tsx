7aeb7a20e0af747182859538a40d9028c7848215
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-12 21:00:30 -0300

    feat(loader): update Loader styles and animations
    - Changed font-family of `.loaderMessage` to 'Silkscreen'.
    - Added `user-select: none` to prevent text selection.
    - Adjusted dimensions and aspect ratio of the `.loader`.
    - Updated keyframe animations for smoother transitions.
    
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/components/Loader/Loader.tsx
+++ frontend/src/components/Loader/Loader.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Loader.module.css';
-import sharedStyles from '../../pages/shared.module.css';
 
 type LoaderProps = {
   className?: string;
@@ -10,7 +9,7 @@ type LoaderProps = {
 
 const Loader: React.FC<LoaderProps> = ({ className = '', style, message }) => (
   <div className={`${styles.centerLoader} ${className}`} style={style}>
-    <div className={sharedStyles.loader}></div>
+    <div className={styles.loader}></div>
     {message && <div className={styles.loaderMessage}>{message}</div>}
   </div>
 );
